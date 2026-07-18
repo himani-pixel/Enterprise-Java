d8e5e91d0ef1026952e428631c48a002b2bcd4f4
Himani Malankar <himanimalankar@gmail.com>
2026-07-18 19:32:16 +0530

    Create Database.sql

---
+++ Practical 1/Practical_1C/Database.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE db;
+
+USE db;
+
+CREATE TABLE user_register (
+    uid CHAR(40),
+    fname CHAR(40),
+    sname CHAR(40),
+    pwd CHAR(40),
+    pwd1 CHAR(40),
+    town CHAR(40),
+    country CHAR(40),
+    zipcode CHAR(40)
+);
