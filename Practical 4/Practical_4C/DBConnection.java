a0859ab37d9ac8c35e776d278b87a1b5e6fd471f
Himani Malankar <himanimalankar@gmail.com>
2026-07-18 14:26:17 +0530

    Create DBConnection.java

---
+++ Practical 4/Practical_4C/DBConnection.java
@@ -0,0 +1,27 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/studentdb",
+                    "root",
+                    "mysql_2026");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
